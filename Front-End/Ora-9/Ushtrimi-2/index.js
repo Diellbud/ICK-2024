c497ed9edab286e42d28c252825babbe38f51511
Diell <budimadiell@gmail.com>
2024-12-14 17:27:55 +0100

    Ora-9 Ushtrimi-2

---
+++ Front-End/Ora-9/Ushtrimi-2/index.js
@@ -0,0 +1,26 @@
+console.log("a") //ni lloj i outputit
+
+
+// 3 menyrat e variablave
+//const
+//let
+//var - nuk perdoret ma
+
+
+   const age = 13 // sakte
+// const Age = 13 // gabim
+
+const firstName = "Diell"
+const lastName = "Budima"
+console.log("Age " + age)
+console.log("First Name: " + firstName)
+console.log("Last Name: " + lastName)
+
+// const variable1 = "Ari"
+// variable1 = "Gimi"
+// gabim
+
+let variable1 = "Ari"
+console.log(variable1)
+variable1 = "Gimi"
+console.log(variable1)
