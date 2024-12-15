196d8d875f9ea3aaa83af0429a297f82ab886834
Diell <budimadiell@gmail.com>
2024-12-15 16:33:23 +0100

    Ora-10 Ushtrimi-3

---
+++ Front-End/Ora-10/Ushtrimi-3/index.js
@@ -0,0 +1,23 @@
+// const a = 5
+// const b = 3
+// const c = a + b
+// const d = a - b
+// const e = b * b
+// const i = b / b
+// const z = a % b
+
+// const isEven = z == 0
+
+const a = 5
+const b = 3
+
+const c = a != b // boolean
+const d = a !== b
+
+const x = null
+const y = undefined
+
+x==y
+x===y
+
+// console.log(isEven)
