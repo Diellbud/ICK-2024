6990b65e7f1d76c669b0f4c34ed757f40860165b
Diell <budimadiell@gmail.com>
2024-12-15 17:12:44 +0100

    Ora-10 Ushtrimi-5

---
+++ Front-End/Ora-10/Ushtrimi-5/index.js
@@ -0,0 +1,21 @@
+const color = 'red'
+
+if(color==='red'){
+    console.log('ndalu')
+} else if(color === 'yellow'){
+    console.log('pergaditu')
+} else if(color === 'green'){
+    console.log('vazhdo')
+}else{
+    console.log('nuk punon semafori')
+}
+const age = 17
+const gender = 'F'
+if(age>=18 && gender==='M'){
+    console.log('Allowed')
+} 
+else if(age>=17 && gender==='F'){
+    console.log('Allowed')
+}else{
+    console.log("You are NOT allowed")
+}
