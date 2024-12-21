5678e13c18cd15ee2bea167bc710554b98e3e751
Diell <budimadiell@gmail.com>
2024-12-21 16:13:53 +0100

    Ora-11 Ushtrimi-2

---
+++ Front-End/Ora-11/Ushtrimi-2/index.js
@@ -0,0 +1,12 @@
+const sum = (numriX, numriY) => {
+    console.log('bbbb')
+    return  numriX + numriY
+}
+const sum1 = (numriX, numriY) => {
+    // nuk kthen vlere, nuk ka return
+    console.log('bbbb', numriX + numriY)
+}
+const x = sum(3,4)
+console.log(x)
+
+
